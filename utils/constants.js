5f5e33305f1eb3e1ed8e214539bbd84638c69725
Soham <sohamcs2003@gmail.com>
2026-07-07 22:03:43 +0530

    refactor for constants

---
+++ utils/constants.js
@@ -0,0 +1,14 @@
+const PROJECT_STATUS = ["Pending", "In Progress", "Completed"];
+
+const TASK_STATUS = ["Pending", "In Progress", "Completed"];
+
+const TASK_PRIORITY = ["Low", "Medium", "High"];
+
+const USER_ROLES = ["Admin", "HOD", "Member"];
+
+module.exports = {
+  PROJECT_STATUS,
+  TASK_STATUS,
+  TASK_PRIORITY,
+  USER_ROLES,
+};
